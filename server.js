8985c6cffd7bdaf7bac4e4c605eeee2e56a7f401
rmoise <roderickfmoise@gmail.com>
2023-02-02 15:50:17 +0100

    add cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const keys = require('./config/keys');
 var cors = require('cors')
-app.use(cors());
+
 
 
 const nodemailer = require('nodemailer');
@@ -16,6 +16,25 @@ app.use(express.json())
 app.get('/', (req, res)=> {
 res.sendFile(__dirname + '/public/contact.html')
 })
+
+const allowedOrigins = [
+    'https://rmoise.github.io/portfolio-website/Public/contact.html',
+];
+
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                // If a specific origin isn’t found on the list of allowed origins
+                let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+                return callback(new Error(message), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
 
 app.post('/', (req, res) =>{
     console.log(req.body);
